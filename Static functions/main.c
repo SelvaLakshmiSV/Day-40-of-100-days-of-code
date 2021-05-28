b9d2425d171e7ed711af0f689a2343699e9e30b1
Selva Lakshmi S.V <76725996+SelvaLakshmiSV@users.noreply.github.com>
2021-05-29 00:00:57 +0530

    File that acees to the function in another file

---
+++ Static functions/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int result;
+    printf("Hai I am (GLOBAL) function!\n");
+    result = fun(4,5);
+    printf("%d",result);
+    return 0;
+}
